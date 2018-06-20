52a21f11246f01da04cbda78d7f56d95d1adbd1d
Tom Groombridge <tom.groombridge@zopa.com>
2018-06-20 15:26:19 +0100

    create rounds in admin

---
+++ app/admin/rounds.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Round do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :score, :player_id, :course_id
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,35 @@
 
 
 
-course = Course.create(name: 'First Course', description: 'Very Nice Course')
-player = Player.create(name: 'Tom G', profile: 'A Shit Player on a Role')
-
-player1 = Player.create(name: 'Broggy', profile: 'Boring Player')
-
-Round.create(player_id: player.id, course_id: course.id, score: 30)
-Round.create(player_id: player.id, course_id: course.id, score: 10)
-Round.create(player_id: player.id, course_id: course.id, score: 40)
-Round.create(player_id: player.id, course_id: course.id, score: 40)
-Round.create(player_id: player.id, course_id: course.id, score: 40)
+Course.create(name: 'Aroeira Course 1', description: 'Very Nice Course')
+Course.create(name: 'Oitavos Dunes', description: 'Very Nice Course')
+Course.create(name: 'Quinta Do Peru', description: 'Very Nice Course')
+Course.create(name: 'Aroeira Course 2', description: 'Very Nice Course')
+Course.create(name: 'Penha Longa Atlantic', description: 'Very Nice Course')
 
 
 
-Round.create(player_id: player1.id, course_id: course.id, score: 30)
-Round.create(player_id: player1.id, course_id: course.id, score: 20)
-Round.create(player_id: player1.id, course_id: course.id, score: 40)
-Round.create(player_id: player1.id, course_id: course.id, score: 10)
-Round.create(player_id: player1.id, course_id: course.id, score: 10)
-Round.create(player_id: player1.id, course_id: course.id, score: 0)
+Player.create(name: 'Tom Groombridge')
+Player.create(name: 'Stephen Brogdan')
+Player.create(name: 'Russell Pirie')
+Player.create(name: 'Jonny Atkins')
+Player.create(name: 'Alun Reese')
+Player.create(name: 'Alistar Brogdan')
+Player.create(name: 'Chris James')
+
+# Round.create(player_id: player.id, course_id: course.id, score: 30)
+# Round.create(player_id: player.id, course_id: course.id, score: 10)
+# Round.create(player_id: player.id, course_id: course.id, score: 40)
+# Round.create(player_id: player.id, course_id: course.id, score: 40)
+# Round.create(player_id: player.id, course_id: course.id, score: 40)
+
+
+
+# Round.create(player_id: player1.id, course_id: course.id, score: 30)
+# Round.create(player_id: player1.id, course_id: course.id, score: 20)
+# Round.create(player_id: player1.id, course_id: course.id, score: 40)
+# Round.create(player_id: player1.id, course_id: course.id, score: 10)
+# Round.create(player_id: player1.id, course_id: course.id, score: 10)
+# Round.create(player_id: player1.id, course_id: course.id, score: 0)
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
