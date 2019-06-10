67ea4b458eeec81fa01d61896b1da6c780d171d8
TomGroombridge <groombridgetom@gmail.com>
2019-06-10 23:29:27 +0100

    added new database

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,37 +9,41 @@
 
 
 
-
-course1 = Course.create(name: 'Aroeira Course 1', description: 'Very Nice Course', date:  Date.parse('08/06/2019'))
-course2 = Course.create(name: 'Oitavos Dunes', description: 'Very Nice Course', date:  Date.parse('09/06/2019'))
-course3 = Course.create(name: 'Quinta Do Peru', description: 'Very Nice Course', date:  Date.parse('10/06/2019'))
-course4 = Course.create(name: 'Aroeira Course 2', description: 'Very Nice Course', date:  Date.parse('11/06/2019'))
-course5 = Course.create(name: 'Penha Longa Atlantic', description: 'Very Nice Course', date:  Date.parse('12/06/2019'))
-
-
-
-player1 = Player.create(name: 'Tom Groombridge')
-player2 = Player.create(name: 'Stephen Brogdan')
-player3 = Player.create(name: 'Russell Pirie')
-player4 = Player.create(name: 'Jonny Atkins')
-player5 = Player.create(name: 'Alun Reese')
-player6 = Player.create(name: 'Alistar Brogdan')
-player7 = Player.create(name: 'Chris James')
-
-
-Team.create(course: course1, players:[player1, player2, player3], score: 90)
-Team.create(course: course1, players:[player4, player5, player6], score: 91)
-
-
-
-Round.create(player: player1, course: course1, score: 30, accumulative_score: 90)
-Round.create(player: player2, course: course1, score: 30, accumulative_score: 80)
-Round.create(player: player3, course: course1, score: 30, accumulative_score: 70)
-Round.create(player: player4, course: course1, score: 30, accumulative_score: 60)
+course1 = Course.create(name: 'Boavista', description: 'Very Nice Course', date:  Date.parse('08/06/2019'))
+course2 = Course.create(name: 'Espiche', description: 'Very Nice Course', date:  Date.parse('09/06/2019'))
+course3 = Course.create(name: 'Penina', description: 'Very Nice Course', date:  Date.parse('10/06/2019'))
+course4 = Course.create(name: 'Palmares', description: 'Very Nice Course', date:  Date.parse('11/06/2019'))
+course5 = Course.create(name: 'Palmares', description: 'Very Nice Course', date:  Date.parse('12/06/2019'))
+course6 = Course.create(name: 'S.Antonio', description: 'Very Nice Course', date:  Date.parse('13/06/2019'))
+course7 = Course.create(name: 'Espiche', description: 'Very Nice Course', date:  Date.parse('14/06/2019'))
+course8 = Course.create(name: 'Boavista', description: 'Very Nice Course', date:  Date.parse('15/06/2019'))
 
 
 
 
+
+player1 = Player.create(name: 'Groomo')
+player2 = Player.create(name: 'Steve')
+player3 = Player.create(name: 'Rusty')
+player4 = Player.create(name: 'Jonny')
+player5 = Player.create(name: 'Alun')
+player6 = Player.create(name: 'Al')
+player7 = Player.create(name: 'CJ')
+player8 = Player.create(name: 'Ed')
+player9 = Player.create(name: 'Dowley')
+
+
+Team.create(course: course1, players:[player3, player5, player4], score: 76)
+Team.create(course: course1, players:[player8, player2, player6], score: 73)
+
+Team.create(course: course2, players:[player6, player8, player9, player5], score: 93)
+Team.create(course: course2, players:[player3, player2, player4, player1], score: 83)
+
+Team.create(course: course3, players:[player6, player3, player8, player9], score: 95)
+Team.create(course: course3, players:[player1, player4, player2, player5], score: 91)
+
+Team.create(course: course4, players:[player8, player5, player1, player2], score: 0)
+Team.create(course: course4, players:[player6, player3, player4, player9], score: 0)
 
 
 
