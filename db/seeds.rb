9aa48fd86f7d4f29a42a9d4dea525b8fb8c41140
TomGroombridge <groombridgetom@gmail.com>
2019-06-12 08:34:37 +0100

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,20 @@
 # course2 = Course.create(name: 'Espiche', description: 'Very Nice Course', date:  Date.parse('09/06/2019'))
 # course3 = Course.create(name: 'Penina', description: 'Very Nice Course', date:  Date.parse('10/06/2019'))
 # course4 = Course.create(name: 'Palmares', description: 'Very Nice Course', date:  Date.parse('11/06/2019'))
-course5 = Course.create(name: 'Palmares', description: 'Very Nice Course', date:  Date.parse('12/06/2019'))
+# course = Course.find(name: 'Palmares', description: 'Very Nice Course', date:  Date.parse('12/06/2019'))
 # course6 = Course.create(name: 'S.Antonio', description: 'Very Nice Course', date:  Date.parse('13/06/2019'))
 # course7 = Course.create(name: 'Espiche', description: 'Very Nice Course', date:  Date.parse('14/06/2019'))
 # course8 = Course.create(name: 'Boavista', description: 'Very Nice Course', date:  Date.parse('15/06/2019'))
+course = Course.find_by(date:  "2019-06-12")
 
-
-player1 = Player.create(name: 'Groomo')
-player2 = Player.create(name: 'Steve')
-player3 = Player.create(name: 'Rusty')
-player4 = Player.create(name: 'Jonny')
-player5 = Player.create(name: 'Alun & CJ')
-player6 = Player.create(name: 'Al')
-player8 = Player.create(name: 'Ed')
-player9 = Player.create(name: 'Dowley')
+player1 = Player.find_by(name: 'Groomo')
+player2 = Player.find_by(name: 'Steve')
+player3 = Player.find_by(name: 'Rusty')
+player4 = Player.find_by(name: 'Jonny')
+player5 = Player.find_by(name: 'Alun & CJ')
+player6 = Player.find_by(name: 'Al')
+player8 = Player.find_by(name: 'Ed')
+player9 = Player.find_by(name: 'Dowley')
 
 
 # Team.create(course: course1, players:[player3, player5, player4], score: 76)
@@ -30,8 +30,8 @@ player9 = Player.create(name: 'Dowley')
 # Team.create(course: course4, players:[player8, player5, player1, player2], score: 97)
 # Team.create(course: course4, players:[player6, player3, player4, player9], score: 107)
 
-Team.create(course: course5, players:[player1, player2, player6, player9], score: 0)
-Team.create(course: course5, players:[player3, player4, player5, player8], score: 0)
+Team.create(course: course, players:[player1, player2, player6, player9], score: 0)
+Team.create(course: course, players:[player3, player4, player5, player8], score: 0)
 
 
 # AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
